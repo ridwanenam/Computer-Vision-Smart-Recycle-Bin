9256b71672f3a3de0b501c1c054dd85edad6e187
ridwanenam <147575561+ridwanenam@users.noreply.github.com>
2024-12-24 08:52:28 +0700

    Update trainYolo.py

--- trainYolo.py
+++ trainYolo.py
@@ -1,15 +1,13 @@
 from ultralytics import YOLO
 
-# Load model YOLOv8 yang sudah pre-trained
-model = YOLO("yolov8n.pt")  # Anda juga bisa mengganti dengan 'yolov8s.pt', dll.
+model = YOLO("yolov8n.pt") 
 
-# Melatih model
 model.train(
-    data="/home/pratama/Desktop/Code1/TubesMikom/Uno-Cards-1/data.yaml",  # Path ke file data.yaml
+    data="/home/pratama/Desktop/Code1/TubesMikom/Uno-Cards-1/data.yaml", 
     epochs=15,                  # Jumlah epoch
-    imgsz=640,                  # Ukuran gambar
+    imgsz=640,                  # Image size
     batch=16,                   # Batch size
-    name="Model_unoCard",   # Nama folder output
-    project="runs/train",       # Lokasi hasil training
-    device= -1                    # Gunakan GPU (0) atau CPU (-1)
+    name="Model_unoCard",       # Folder output
+    project="runs/train",       # Output hasil training
+    device= -1                  # GPU (0) atau CPU (-1)
 )
